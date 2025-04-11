dc80030331cfd16ea155fdadd7b32d4eb29784bd
hyunsik <hora5225@gmail.com>
2025-04-11 17:49:34 +0900

    CORS 로컬호스트3000으로 변경

--- tetris/backend/main.go
+++ tetris/backend/main.go
@@ -118,7 +118,7 @@ func main() {
 
 	// CORS 미들웨어 추가
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8081", "http://127.0.0.1:8081"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
